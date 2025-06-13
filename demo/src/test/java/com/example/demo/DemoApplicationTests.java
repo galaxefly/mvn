6a7a0471d26f72a18c74b4d7494812bc88c2623f
galaxefly <gdulatova4@gmail.com>
2025-06-13 18:17:20 +0300

    Added Tests

--- demo/src/test/java/com/example/demo/DemoApplicationTests.java
+++ demo/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -34,5 +34,59 @@ class DemoApplicationTests {
         }
     }
 
+    //Проверка заголовка страницы
+    @Test
+    public void testPageTitle() {
+        String title = driver.getTitle();
+        assertEquals("Maven Repository: Search/Browse/Explore", title);
+    }
+
+    //Проверка наличия меню навигации
+    @Test
+    public void testNavigationMenuPresence() {
+        WebElement nav = driver.findElement(By.className("page"));
+        assertTrue(nav.isEnabled());
+    }
+
+    //Проверка кнопки Search на главной странице
+    @Test
+    public void testRunTestsButton() {
+        WebElement runButton = driver.findElement(By.id("search"));
+        assertTrue(runButton.isDisplayed());
+
+        runButton.click();
+        WebElement resultsBlock = driver.findElement(By.id("search"));
+        assertTrue(resultsBlock.isDisplayed());
+
+        String currentUrl = driver.getCurrentUrl();
+        assertFalse("URL не изменился после клика", currentUrl.contains("https://mvnrepository.com/search?q="));
+    }
+
+    //Проверка заполнения текстового поля
+    @Test
+    public void testFillTextField() {
+        WebElement searchInput = driver.findElement(By.id("query"));
+        // Проверяем, что поле отображается и доступно для ввода
+        assertTrue("Поле поиска не отображается", searchInput.isDisplayed());
+        assertTrue("Поле поиска недоступно для ввода", searchInput.isEnabled());
+
+        // Вводим текст
+        String testText = "selenium";
+        searchInput.sendKeys(testText);
+
+        // Проверяем, что текст успешно введен
+        String enteredText = searchInput.getAttribute("value");
+        assertEquals("Введенный текст не совпадает", testText, enteredText);
+    }
+
+    //Проверка перехода по ссылке нажатием на пункт меню Popular
+    @Test
+    public void NavigateToPopularTest() {
+        WebElement popularLink = driver.findElement(By.linkText("Popular"));
+        popularLink.click();
+
+        String currentUrl = driver.getCurrentUrl();
+        assertEquals("URL после перехода не совпадает", "https://mvnrepository.com/popular", currentUrl);
+    }
 
 }
